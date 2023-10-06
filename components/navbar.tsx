48aee1edc178c0fad36a818297784a18c46106c0
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-10-06 17:30:26 +0200

    Add settings/ log out to user icon menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,15 @@ import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import UserAvatar from './userAvatar';
 import { useUser } from '@/hooks/useUser';
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+} from './ui/navigation-menu';
+import Link from 'next/link';
 
 const Navbar = () => {
 	const { user, userProfile } = useUser();
@@ -18,12 +27,45 @@ const Navbar = () => {
 			/>
 			{user && (
 				<div className="flex w-full justify-end">
-					<UserAvatar />
-					<form action="/auth/signout" method="post">
-						<Button type="submit" variant="outline">
-							Sign out
-						</Button>
-					</form>
+					<NavigationMenu>
+						<NavigationMenuList>
+							<NavigationMenuItem>
+								<NavigationMenuTrigger>
+									<UserAvatar />
+								</NavigationMenuTrigger>
+								<NavigationMenuContent>
+									<ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+										<li>
+											<NavigationMenuLink asChild>
+												<Link
+													href="/settings"
+													className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+												>
+													Settings
+												</Link>
+											</NavigationMenuLink>
+										</li>
+										<li>
+											<NavigationMenuLink asChild>
+												<form
+													action="/auth/signout"
+													method="post"
+													className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+												>
+													<Button
+														type="submit"
+														variant="outline"
+													>
+														Sign out
+													</Button>
+												</form>
+											</NavigationMenuLink>
+										</li>
+									</ul>
+								</NavigationMenuContent>
+							</NavigationMenuItem>
+						</NavigationMenuList>
+					</NavigationMenu>
 				</div>
 			)}
 		</div>
